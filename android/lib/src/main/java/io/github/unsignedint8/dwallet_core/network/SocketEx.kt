a9b4b2015e7e968977ebc1518c2f45397cbd2594
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-14 16:32:25 +0800

    read/write

--- android/lib/src/main/java/io/github/unsignedint8/dwallet_core/network/SocketEx.kt
+++ android/lib/src/main/java/io/github/unsignedint8/dwallet_core/network/SocketEx.kt
@@ -1,18 +1,14 @@
 package io.github.unsignedint8.dwallet_core.network
 
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.async
-import java.io.IOException
+import kotlinx.coroutines.experimental.*
 import java.net.InetSocketAddress
 import java.net.Socket
-import java.util.concurrent.TimeoutException
 
 /**
  * Created by unsignedint8 on 8/14/17.
  */
 
-class SocketEx() : Socket() {
+class SocketEx : Socket() {
 
     var lastException: Exception? = null
 
@@ -32,5 +28,20 @@ class SocketEx() : Socket() {
         return async(CommonPool) { connect(host, port, timeout) }
     }
 
+    fun read(size: Int = DEFAULT_BUFFER_SIZE): ByteArray {
+        return this.inputStream.readBytes(size)
+    }
 
+    suspend fun readAsync(size: Int = DEFAULT_BUFFER_SIZE): Deferred<ByteArray> {
+        return async(CommonPool) { read(size) }
+    }
+
+    fun write(data: ByteArray): Int {
+        this.outputStream.write(data)
+        return data.size
+    }
+
+    fun writeAsync(data: ByteArray): Deferred<Int> {
+        return async(CommonPool) { write(data) }
+    }
 }
--- android/lib/src/test/java/io/github/unsignedint8/dwallet_core/SocketTests.kt
+++ android/lib/src/test/java/io/github/unsignedint8/dwallet_core/SocketTests.kt
@@ -6,9 +6,13 @@ package io.github.unsignedint8.dwallet_core
 
 
 import io.github.unsignedint8.dwallet_core.network.SocketEx
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
 import org.junit.Assert.*
+import java.net.ServerSocket
 
 class SocketTests {
     @Test
@@ -23,5 +27,22 @@ class SocketTests {
         val s = SocketEx()
         assert(s.connectAsync("baidu.com", 80).await())
         assertEquals(null, s.lastException)
+    }
+
+    @Test
+    fun testWrite() = runBlocking {
+
+        async(CommonPool) {
+            val server = ServerSocket(9999)
+            val client = server.accept()
+            val msg = client.getInputStream().bufferedReader().readLine()
+            assertEquals("hello\n", msg)
+        }
+
+        delay(1000)
+        val c1 = SocketEx()
+        assert(c1.connectAsync("localhost", 9999).await())
+        assertEquals(6, c1.writeAsync("hello\n".toByteArray()).await())
+        delay(1000)
     }
 }
