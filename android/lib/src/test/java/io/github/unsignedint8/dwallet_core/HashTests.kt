a830d87af793bbf0ea876c9819b995bd8bf44569
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-17 16:35:17 +0800

    add test cases

--- android/lib/src/test/java/io/github/unsignedint8/dwallet_core/HashTests.kt
+++ android/lib/src/test/java/io/github/unsignedint8/dwallet_core/HashTests.kt
@@ -115,5 +115,45 @@ class HashTests {
         assert(f.contains("b9300670b4c5366e95b2699e8b18bc75e5f729c5".hexToByteArray()))
 
         assertArrayEquals(byteArrayOf(97, 78, 155.toByte()), f.data)
+        assertEquals(5, f.nHashFuncs)
+        assertEquals(0, f.nTweak)
+        assertEquals(1, f.nFlags)
+    }
+
+    @Test
+    fun testBloomFilterTweak() {
+        val f = BloomFilter.create(3, 0.01, 2147483649.toInt(), BloomFilter.BLOOM_UPDATE_ALL)
+        f.insert("99108ad8ed9bb6274d3980bab5a85c048f0950c8".hexToByteArray())
+        assert(f.contains("99108ad8ed9bb6274d3980bab5a85c048f0950c8".hexToByteArray()))
+
+        assert(!f.contains("19108ad8ed9bb6274d3980bab5a85c048f0950c8".hexToByteArray()))
+
+        f.insert("b5a2c786d9ef4658287ced5914b37a1b4aa32eee".hexToByteArray())
+        assert(f.contains("b5a2c786d9ef4658287ced5914b37a1b4aa32eee".hexToByteArray()))
+
+        f.insert("b9300670b4c5366e95b2699e8b18bc75e5f729c5".hexToByteArray())
+        assert(f.contains("b9300670b4c5366e95b2699e8b18bc75e5f729c5".hexToByteArray()))
+
+        assertArrayEquals(byteArrayOf(206.toByte(), 66, 153.toByte()), f.data)
+        assertEquals(5, f.nHashFuncs)
+        assertEquals(2147483649.toInt(), f.nTweak)
+        assertEquals(1, f.nFlags)
+    }
+
+    @Test
+    fun testBloomFilterPubkey() {
+        val f = BloomFilter.create(2, 0.001, 0, BloomFilter.BLOOM_UPDATE_ALL)
+        f.insert("045b81f0017e2091e2edcd5eecf10d5bdd120a5514cb3ee65b8447ec18bfc4575c6d5bf415e54e03b1067934a0f0ba76b01c6b9ab227142ee1d543764b69d901e0".hexToByteArray())
+        f.insert("477abbacd4113f2e6b100526222eedd953c26a64".hexToByteArray())
+
+        assertArrayEquals(byteArrayOf(143.toByte(), 193.toByte(), 107), f.data)
+        assertEquals(8, f.nHashFuncs)
+        assertEquals(0, f.nTweak)
+    }
+
+    @Test
+    fun testBloomFilterThreshold() {
+        val f = BloomFilter.create(10, 0.0000000000000001)
+        assertEquals(BloomFilter.MAX_HASH_FUNCS, f.nHashFuncs)
     }
 }
