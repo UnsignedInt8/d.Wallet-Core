f1dc0c2ca02fb25e5e9bfcc9dc63e5192a3470d1
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-17 11:32:06 +0800

    fix murmurhash bugs

--- android/lib/src/test/java/io/github/unsignedint8/dwallet_core/HashTests.kt
+++ android/lib/src/test/java/io/github/unsignedint8/dwallet_core/HashTests.kt
@@ -30,20 +30,25 @@ class HashTests {
     @Test
     fun testMurmurhash() {
         val cases = arrayOf(
-//                Triple(0x00000000.toLong(), 0x00000000.toLong(), byteArrayOf()),
-//                Triple(0x6a396f08.toLong(), 0xFBA4C795.toLong(), byteArrayOf()),
-                Triple(0x81f16f39.toLong(), 0xffffffff.toLong(), byteArrayOf())
-//                Triple(0x514e28b7.toLong(), 0x00000000.toLong(), byteArrayOf(0x00)),
-//                Triple(0xea3f0b17.toLong(), 0xFBA4C795.toLong(), byteArrayOf(0x00)),
-//                Triple(0xfd6cf10d.toLong(), 0x00000000.toLong(), byteArrayOf(0xff.toByte())),
-//                Triple(0x16c6b7ab.toLong(), 0x00000000.toLong(), byteArrayOf(0x00, 0x11)),
-//                Triple(0x8eb51c3d.toLong(), 0x00000000.toLong(), byteArrayOf(0x00, 0x11, 0x22)),
-//                Triple(0xb4471bf8.toLong(), 0x00000000.toLong(), byteArrayOf(0x00, 0x11, 0x22, 0x33))
+//                Triple(0x00000000.toInt(), 0x00000000.toInt(), byteArrayOf()),
+//                Triple(0x6a396f08.toInt(), 0xFBA4C795.toInt(), byteArrayOf()),
+//                Triple(0x81f16f39.toInt(), 0xffffffff.toInt(), byteArrayOf()),
+//                Triple(0x514e28b7.toInt(), 0x00000000.toInt(), byteArrayOf(0x00)),
+//                Triple(0xea3f0b17.toInt(), 0xFBA4C795.toInt(), byteArrayOf(0x00)),
+                Triple(0xfd6cf10d.toInt(), 0x00000000.toInt(), byteArrayOf(0xff.toByte())),
+                Triple(0x16c6b7ab.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11)),
+                Triple(0x8eb51c3d.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11, 0x22)),
+                Triple(0xb4471bf8.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11, 0x22, 0x33)),
+                Triple(0xe2301fa8.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11, 0x22, 0x33, 0x44)),
+                Triple(0xfc2e4a15.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11, 0x22, 0x33, 0x44, 0x55)),
+                Triple(0xb074502c.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66)),
+                Triple(0x8034d2a0.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77)),
+                Triple(0xb4698def.toInt(), 0x00000000.toInt(), byteArrayOf(0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88.toByte()))
         )
 
         cases.forEach {
-            println(it.first.toInt64LEBytes().toHexString())
-            println(murmurHash3(it.second.toInt(), it.third).toInt32LEBytes().toHexString())
+            println(it.first.toInt32LEBytes().toHexString())
+            println(murmurHash3(it.second, it.third).toInt().toInt32LEBytes().toHexString())
         }
     }
 }
