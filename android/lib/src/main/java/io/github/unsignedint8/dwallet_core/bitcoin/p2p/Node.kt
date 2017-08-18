87a707cbe2fe958535d1b81bbb10d8ef45d8d2d0
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-18 10:46:30 +0800

    add event class

---
+++ android/lib/src/main/java/io/github/unsignedint8/dwallet_core/bitcoin/p2p/Node.kt
@@ -0,0 +1,11 @@
+package io.github.unsignedint8.dwallet_core.bitcoin.p2p
+
+import io.github.unsignedint8.dwallet_core.infrastructure.Event
+
+/**
+ * Created by unsignedint8 on 8/18/17.
+ */
+
+class Node : Event() {
+
+}
---
+++ android/lib/src/main/java/io/github/unsignedint8/dwallet_core/infrastructure/Event.kt
@@ -0,0 +1,28 @@
+package io.github.unsignedint8.dwallet_core.infrastructure
+
+/**
+ * Created by unsignedint8 on 8/18/17.
+ */
+
+typealias Callback = (sender: Any, params: Any) -> Unit
+
+open class Event {
+
+    protected val observers = mutableMapOf<String, MutableList<Callback>>()
+
+    protected fun register(event: String, callback: Callback) {
+        var list = observers[event]
+        if (list == null) {
+            list = mutableListOf()
+            observers[event] = list
+        }
+
+        list.add(callback)
+    }
+
+    protected fun trigger(event: String, sender: Any, params: Any) {
+        observers[event]?.forEach {
+            it.invoke(sender, params)
+        }
+    }
+}
