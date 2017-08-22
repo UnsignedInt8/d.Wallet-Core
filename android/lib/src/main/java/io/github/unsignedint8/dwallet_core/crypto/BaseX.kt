c91a642a0fbda98c810d601490f72fd0667bdb9b
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-22 21:57:08 +0800

    test basex

--- android/lib/src/main/java/io/github/unsignedint8/dwallet_core/crypto/BaseX.kt
+++ android/lib/src/main/java/io/github/unsignedint8/dwallet_core/crypto/BaseX.kt
@@ -1,32 +1,71 @@
 package io.github.unsignedint8.dwallet_core.crypto
 
+import io.github.unsignedint8.dwallet_core.extensions.ZERO
+import io.github.unsignedint8.dwallet_core.extensions.toBigInteger
+
 /**
  * Created by unsignedint8 on 8/22/17.
  * https://github.com/cryptocoinjs/base-x/blob/master/index.js
  */
 
-class BaseX(val alpha: String) {
+class BaseX(val alphabet: String) {
 
     private val alphaMap = mutableMapOf<Char, Int>()
-    private val base = alpha.length
-    private val leader = alpha.first()
+    private val base = alphabet.length
+    private val leader = alphabet.first()
 
     init {
-        alpha.forEachIndexed { index, c -> alphaMap[c] = index }
+        alphabet.forEachIndexed { index, c -> alphaMap[c] = index }
     }
 
-    
+    fun encode(source: ByteArray): String {
+
+        if (source.isEmpty()) return ""
+
+        val digits = mutableListOf(0)
+
+        source.forEachIndexed { i, byte ->
+            var carry = byte.toBigInteger().toInt()
+
+            digits.forEachIndexed { j, digit ->
+                carry += digit shl 8
+                digits[j] = carry % base
+                carry /= base
+            }
+
+            while (carry > 0) {
+                digits.add(carry % base)
+                carry /= base
+            }
+        }
+
+        var string = ""
+
+        var k = 0
+        while (k < source.size && source[k] == Byte.ZERO) {
+            string += alphabet[0]
+            ++k
+        }
+
+        var q = digits.size - 1
+        while (q >= 0) {
+            string += alphabet[digits[q]]
+            --q
+        }
+
+        return string
+    }
 
     companion object {
-        const val base2 = "01"
-        const val base8 = "01234567"
-        const val base11 = "0123456789a"
-        const val base16 = "0123456789abcdef"
-        const val base32 = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
-        const val base36 = "0123456789abcdefghijklmnopqrstuvwxyz"
-        const val base58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-        const val base62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        const val base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-        const val base66 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.!~"
+        val base2 by lazy { BaseX("01") }
+        val base8 by lazy { BaseX("01234567") }
+        val base11 by lazy { BaseX("0123456789a") }
+        val base16 by lazy { BaseX("0123456789abcdef") }
+        val base32 by lazy { BaseX("0123456789ABCDEFGHJKMNPQRSTVWXYZ") }
+        val base36 by lazy { BaseX("0123456789abcdefghijklmnopqrstuvwxyz") }
+        val base58 by lazy { BaseX("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz") }
+        val base62 by lazy { BaseX("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") }
+        val base64 by lazy { BaseX("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/") }
+        val base66 by lazy { BaseX("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.!~") }
     }
 }
--- android/lib/src/test/java/io/github/unsignedint8/dwallet_core/Ripemd160Tests.kt
+++ android/lib/src/test/java/io/github/unsignedint8/dwallet_core/Ripemd160Tests.kt
@@ -25,7 +25,7 @@ class Ripemd160Tests {
     }
 
     @Test
-    fun testHash160(){
+    fun testHash160() {
         val input = "0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6".hexToByteArray()
 
         val sha256 = sha256(input)
@@ -35,5 +35,30 @@ class Ripemd160Tests {
         assertEquals("010966776006953D5567439E5E39F86A0D273BEE".toLowerCase(), hash160.toHexString())
 
         assertEquals("010966776006953D5567439E5E39F86A0D273BEE".toLowerCase(), hash160(input).toHexString())
+    }
+
+    @Test
+    fun testBase58() {
+        assertEquals("RPGNSU3bqTX", BaseX.base58.encode("91c81cbfdd58bbd2".hexToByteArray()))
+        assertEquals("01111", BaseX.base2.encode("000f".hexToByteArray()))
+        assertEquals("011111111", BaseX.base2.encode("00ff".hexToByteArray()))
+        assertEquals("111111111111", BaseX.base2.encode("0fff".hexToByteArray()))
+        assertEquals("11111111000000001111111100000000", BaseX.base2.encode("ff00ff00".hexToByteArray()))
+        assertEquals("000f", BaseX.base16.encode("0000000f".hexToByteArray()))
+        assertEquals("0fff", BaseX.base16.encode("000fff".hexToByteArray()))
+        assertEquals("", BaseX.base58.encode("".hexToByteArray()))
+        assertEquals("2g", BaseX.base58.encode("61".hexToByteArray()))
+        assertEquals("a3gV", BaseX.base58.encode("626262".hexToByteArray()))
+        assertEquals("aPEr", BaseX.base58.encode("636363".hexToByteArray()))
+        assertEquals("2cFupjhnEsSn59qHXstmK2ffpLv2", BaseX.base58.encode("73696d706c792061206c6f6e6720737472696e67".hexToByteArray()))
+        assertEquals("1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L", BaseX.base58.encode("00eb15231dfceb60925886b67d065299925915aeb172c06647".hexToByteArray()))
+        assertEquals("ABnLTmg", BaseX.base58.encode("516b6fcd0f".hexToByteArray()))
+        assertEquals("3SEo3LWLoPntC", BaseX.base58.encode("bf4f89001e670274dd".hexToByteArray()))
+        assertEquals("3EFU7m", BaseX.base58.encode("572e4794".hexToByteArray()))
+        assertEquals("EJDM8drfXA6uyA", BaseX.base58.encode("ecac89cad93923c02321".hexToByteArray()))
+        assertEquals("Rt5zm", BaseX.base58.encode("10c8511e".hexToByteArray()))
+        assertEquals("FPBt6CHo3fovdL", BaseX.base58.encode("ffffffffffffffffffff".hexToByteArray()))
+        assertEquals("YcVfxkQb6JRzqk5kF2tNLv", BaseX.base58.encode("ffffffffffffffffffffffffffffffff".hexToByteArray()))
+        assertEquals("5Hx15HFGyep2CfPxsJKe2fXJsCVn5DEiyoeGGF6JZjGbTRnqfiD", BaseX.base58.encode("801184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd206ec97e".hexToByteArray()))
     }
 }
