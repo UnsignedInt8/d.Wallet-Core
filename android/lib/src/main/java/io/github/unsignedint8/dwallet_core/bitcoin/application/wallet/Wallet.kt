39fc2ef2b8245f9675e584b3f97fb275baf29bb0
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-25 21:54:12 +0800

    support change keys

--- android/lib/src/main/java/io/github/unsignedint8/dwallet_core/bitcoin/application/wallet/Wallet.kt
+++ android/lib/src/main/java/io/github/unsignedint8/dwallet_core/bitcoin/application/wallet/Wallet.kt
@@ -3,7 +3,7 @@ package io.github.unsignedint8.dwallet_core.bitcoin.application.wallet
 import io.github.unsignedint8.dwallet_core.bitcoin.application.bip32.*
 import io.github.unsignedint8.dwallet_core.bitcoin.application.bip39.*
 import io.github.unsignedint8.dwallet_core.bitcoin.protocol.structures.*
-import io.github.unsignedint8.dwallet_core.bitcoin.script.Interpreter
+import io.github.unsignedint8.dwallet_core.bitcoin.script.*
 import io.github.unsignedint8.dwallet_core.infrastructure.*
 import java.security.SecureRandom
 
@@ -12,9 +12,10 @@ import java.security.SecureRandom
  * Created by unsignedint8 on 8/24/17.
  */
 
-open class Wallet private constructor(val masterXprvKey: ExtendedKey, privKeys: List<ExtendedKey> = listOf(), val coin: CoinType = CoinType.Bitcoin) : Event() {
+open class Wallet private constructor(val masterXprvKey: ExtendedKey, externalKeys: List<ExtendedKey>, changeKeys: List<ExtendedKey>, val coin: CoinType = CoinType.Bitcoin) : Event() {
 
-    val childrenPrivateKeys = mutableListOf<ExtendedKey>()
+    val externalPrivKeys = mutableListOf<ExtendedKey>()
+    val changePrivKeys = mutableListOf<ExtendedKey>()
     private val cachedTxs = mutableMapOf<String, Transaction>()
     private val utxos = mutableMapOf<String, Transaction>()
 
@@ -23,11 +24,16 @@ open class Wallet private constructor(val masterXprvKey: ExtendedKey, privKeys: 
      * m / purpose' / coin_type' / account' / change / address_index
      */
     init {
-        privKeys.forEach { childrenPrivateKeys.add(it) }
+        externalKeys.forEach { externalPrivKeys.add(it) }
 
-        if (privKeys.size < childrenKeysAmount) {
-            val begin = privKeys.size
-            (begin..childrenKeysAmount).mapTo(childrenPrivateKeys) { masterXprvKey.derive(44).derive(coin.raw).derive(0).derive(0).derive(it) }
+        if (externalKeys.size < externalKeysAmount) {
+            val begin = externalKeys.size
+            (begin..externalKeysAmount).mapTo(externalPrivKeys) { masterXprvKey.derive(44).derive(coin.raw).derive(0).derive(0).derive(it) }
+        }
+
+        if (changeKeys.size < changeKeysAmount) {
+            val begin = changeKeys.size
+            (begin..changeKeysAmount).mapTo(changePrivKeys) { masterXprvKey.derive(44).derive(coin.raw).derive(0).derive(1).derive(it) }
         }
     }
 
@@ -41,14 +47,15 @@ open class Wallet private constructor(val masterXprvKey: ExtendedKey, privKeys: 
             return fromMasterXprvKey(masterPrivateKey, coin = coin)
         }
 
-        fun fromMasterXprvKey(masterPrivateKey: ExtendedKey, childrenPrivateKeys: List<ExtendedKey> = listOf(), coin: CoinType = CoinType.Bitcoin) = Wallet(masterPrivateKey, childrenPrivateKeys, coin)
+        fun fromMasterXprvKey(masterPrivateKey: ExtendedKey, externalKeys: List<ExtendedKey> = listOf(), changeKeys: List<ExtendedKey> = listOf(), coin: CoinType = CoinType.Bitcoin) = Wallet(masterPrivateKey, externalKeys, changeKeys, coin)
 
-        fun fromMasterXprvKey(masterPrivateKey: String, childrenPrivateKeys: List<String> = listOf(), coin: CoinType = CoinType.Bitcoin): Wallet? {
+        fun fromMasterXprvKey(masterPrivateKey: String, externalKeys: List<String> = listOf(), changeKeys: List<String> = listOf(), coin: CoinType = CoinType.Bitcoin): Wallet? {
             return try {
                 val master = ExtendedKey.parse(masterPrivateKey, true)
-                val children = childrenPrivateKeys.map { ExtendedKey.parse(it, true) }
+                val external = externalKeys.map { ExtendedKey.parse(it, true) }
+                val change = changeKeys.map { ExtendedKey.parse(it, true) }
 
-                fromMasterXprvKey(master, children, coin)
+                fromMasterXprvKey(master, external, change, coin)
             } catch (e: Exception) {
                 null
             }
@@ -59,7 +66,8 @@ open class Wallet private constructor(val masterXprvKey: ExtendedKey, privKeys: 
             return fromMnemonic(mnemonic, passphrase, coin)
         }
 
-        var childrenKeysAmount = 100
+        var externalKeysAmount = 50
+        var changeKeysAmount = 50
     }
 
     fun insertTransaction(tx: Transaction) {
@@ -68,14 +76,12 @@ open class Wallet private constructor(val masterXprvKey: ExtendedKey, privKeys: 
 
     }
 
-    fun isValidTx(tx: Transaction): Boolean {
-        return tx.txOuts.any { out ->
-            val ops = Interpreter.scriptToOps(out.pubkeyScript)
+    fun isValidTx(tx: Transaction) = tx.txOuts.any { out ->
+        val ops = Interpreter.scriptToOps(out.pubkeyScript)
 
-            if (!Interpreter.isP2PKHOutScript(ops.map { it.first })) return@any false
-            if (ops[2].second == null) return@any false
+        if (!Interpreter.isP2PKHOutScript(ops.map { it.first })) return@any false
+        if (ops[2].second == null) return@any false
 
-            return@any childrenPrivateKeys.any { it.ecKey!!.publicKeyHash!!.contentEquals(ops[2].second!!) }
-        }
+        return@any externalPrivKeys.any { it.ecKey!!.publicKeyHash!!.contentEquals(ops[2].second!!) } || changePrivKeys.any { it.ecKey!!.publicKeyHash!!.contentEquals(ops[2].second!!) }
     }
 }
